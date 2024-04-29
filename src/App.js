0b1a40283cb2012f636ee2c26e4ce4313ffb60d9
Dave McPherson <130245469+suoidix@users.noreply.github.com>
2024-04-29 16:09:11 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Navbar from './Navbar';
+import Home from './Home';
+import Mission from './Mission';
+import Contact from './Contact';
+import Gallery from './Gallery';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        {<Header />}
+        {<Navbar />}
+      </div>
+      <div className="row">
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/mission' element={<Mission />}/>
+          <Route path='/gallery' element={<Gallery />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
