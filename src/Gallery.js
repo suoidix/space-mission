76386de37589db1456565d9aaa6206ed91668235
DaveM WindowsPC <130245469+suoidix@users.noreply.github.com>
2024-04-29 16:35:49 -0400

    move api key to .env

--- src/Gallery.js
+++ src/Gallery.js
@@ -11,7 +11,6 @@ function Gallery() {
     useEffect(() => {
         const apiKey = process.env.REACT_APP_NASA_API_KEY;
 
-
         const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`;
 
         setUrl(apiUrl);
